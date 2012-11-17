2a156e17edf7ffbda37ac1bce4f05f7253969c24
jornamo <j_nobelius@yahoo.se>
2012-11-17 05:40:00 -0800

    Added example of how to customize radio buttons with som dummy images for now

--- application/views/start_view/start_view.php
+++ application/views/start_view/start_view.php
@@ -1,5 +1,12 @@
 <div id="wrapper">
 	<div id="container">
-		
+		<span>
+			Radio1
+			<input type="radio" name="test" />
+		</span>
+		<span>
+			Radio2
+			<input type="radio" name="test" />
+		</span>
 	</div>
 </div>
