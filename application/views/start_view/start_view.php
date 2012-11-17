45afbe79cbdcca48d29c1ef8d7c64719ffe07cdf
jornamo <j_nobelius@yahoo.se>
2012-11-17 14:07:54 -0800

    Added sticky header

--- application/views/start_view/start_view.php
+++ application/views/start_view/start_view.php
@@ -1,32 +1,38 @@
 <div id="wrapper">
 	<div id="container">
-	<h1>Radio knappar / select meny exempel</h1>
-		<select id="lang">
-			<option value="sv">Svenska</option>
-			<option value="en">Engelska</option>
-			<option value="it">Italienska</option>
-			<option value="fr">Franska</option>
-			<option value="de">Tyska</option>
-		</select>
-		
-		<span>
-		Radio 1
-			<input type="radio" name="test" />
-		</span>
-		
-		<span>
-		Radio 2
-			<input type="radio" name="test" />
-		</span>
-		<hr>
-		<span>
-		Radio 3
-			<input type="radio" name="test2" />
-		</span>
-		
-		<span>
-		Radio 4
-			<input type="radio" name="test2" />
-		</span>
+	
+	<div id="header-wrapper">
+		<div id="header"><h1>Sticky header</h1></div>
+	</div>
+	<div>
+		<h1>Radio knappar / select meny exempel</h1>
+			<select id="lang">
+				<option value="sv">Svenska</option>
+				<option value="en">Engelska</option>
+				<option value="it">Italienska</option>
+				<option value="fr">Franska</option>
+				<option value="de">Tyska</option>
+			</select>
+			
+			<span>
+			Radio 1
+				<input type="radio" name="test" />
+			</span>
+			
+			<span>
+			Radio 2
+				<input type="radio" name="test" />
+			</span>
+			<hr>
+			<span>
+			Radio 3
+				<input type="radio" name="test2" />
+			</span>
+			
+			<span>
+			Radio 4
+				<input type="radio" name="test2" />
+			</span>
+		</div>
 	</div>
 </div>
